ca9533216af272f3c2b5ac3a53f091a1753338f5
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-09-02 18:21:59 -0300

    Funcionalidade de selecionar assentos adicionada

--- src/components/pages/SeatsPage/SeatsPage.js
+++ src/components/pages/SeatsPage/SeatsPage.js
@@ -8,6 +8,7 @@ import PageTitle from "../../PageTitle"
 export default function SeatsPage() {
     const params = useParams();
     const [seats, setSeats] = useState("");
+    const [selectedSeats, setSelectedSeats] = useState([]); 
 
     useEffect(() => {
         getSessionSeats(params.idSession, setSeats);
@@ -18,6 +19,26 @@ export default function SeatsPage() {
             <LoadingComponent/>
         )
     }
+
+    function seatAvailable(seatId) {
+        if (selectedSeats.includes(seatId)) {
+            return false; //assento já selecionado, precisa deselecionar
+        }
+        return true; //assento não selecionado, selecionar
+    }
+
+    function toggleSeat(seatId) {
+        if (seatAvailable(seatId)) { 
+            setSelectedSeats([...selectedSeats,seatId]);
+            return "selected-type"
+        }
+        else { 
+            setSelectedSeats(selectedSeats.filter(selectedSeat => selectedSeat !== seatId));
+            return "available-type"
+        }
+    }
+
+
     return (
         <main className="main-content">
             <PageTitle>Selecione o(s) assento(s)</PageTitle>
@@ -25,23 +46,43 @@ export default function SeatsPage() {
             <section className="seats">
                 {
                     seats.seats.map(seat => (
-                        <Seat id={seat.id} name={seat.name} isAvailable={seat.idAvailable}/>
+                        <Seat toggleSeat={toggleSeat} seatAvailable={seatAvailable} id={seat.id} name={seat.name} isAvailable={seat.isAvailable}/>
                     ))
                 }
             </section>
+
+            <section className="status-types">
+                <div>
+                    <div class="status-type-ball selected-type"></div>
+                    <p>Selecionado</p>
+                </div>
+                <div>
+                    <div class="status-type-ball available-type"></div>
+                    <p>Disponível</p>
+                </div>
+                <div>
+                    <div class="status-type-ball unavailable-type"></div>
+                    <p>Indisponível</p>
+                </div>
+            </section>
+            
             
         </main>
      
     )
 }
 
-function Seat({id,name,isAvailable}) {
-    function checkAvailability() {
+function Seat({id,isAvailable,name,toggleSeat,seatAvailable}) {
 
+    const [seatState, setSeatState] = useState("available-type");
+    function reserve() {
+        if (!isAvailable) return alert("Assento já reservado");
+        setSeatState(toggleSeat(id));
     }
+
     return (
-        <div className="seat">
-            <p>{name}</p>
+        <div onClick={reserve} className={"seat " + seatState}>
+            <span>{name}</span>
         </div>
     )
 }
