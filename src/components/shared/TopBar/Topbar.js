625c71b917332503d7fbd39d21c4a792ccd48485
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-09-04 14:49:30 -0300

    sumindo goBack button quando na rota /

--- src/components/shared/TopBar/Topbar.js
+++ src/components/shared/TopBar/Topbar.js
@@ -1,19 +1,24 @@
 import "./Topbar.css"
-import { Link, useHistory } from "react-router-dom"
+import { Link, useHistory, useLocation } from "react-router-dom"
 import styled from "styled-components"
 
 
 
 export default function Topbar() {
     const history = useHistory();
+    const location = useLocation();
 
     return (
         <header class="topbar">
+            {
+                //hook para retornar a rota atual
+                location.pathname === "/" ?
+                "" :
+                //hook goBack para retornar a ultima rota
                 <IconContainer onClick={history.goBack}>
                     <ion-icon name="arrow-back-circle-outline"></ion-icon>
                 </IconContainer>
-
-            
+            }
             <Link to="/">
                 <h1>CINEFLEX</h1>
             </Link>
