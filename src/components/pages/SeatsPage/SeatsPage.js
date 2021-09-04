935773d728f60ad5420867e9dba74232b54979d1
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-09-03 22:39:07 -0300

    Adicionado validação de dados pagina SeatsPage

--- src/components/pages/SeatsPage/SeatsPage.js
+++ src/components/pages/SeatsPage/SeatsPage.js
@@ -5,28 +5,25 @@ import { getSessionSeats, postSeats } from "../../../api"
 import LoadingComponent from "../../LoadingComponent"
 import PageTitle from "../../PageTitle"
 import Footer from "../../Footer"
+import styled from "styled-components"
 
 export default function SeatsPage({setFinalInfos}) {
     const params = useParams();
     const [seats, setSeats] = useState("");
     const [selectedSeats, setSelectedSeats] = useState([]); 
     const [inputValues, setInputValues] = useState({});
-    let history = useHistory();
+    const regexCPF = /^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/
+    const regexNOME = /^[a-záàâãéèêíïóôõöúçñ ]+$/i;
+    const history = useHistory();
 
     useEffect(() => {
         getSessionSeats(params.idSession, setSeats);
     },[])
     
-    if (!seats) {
-        return (
-            <LoadingComponent/>
-        )
-    }
+    if (!seats) { return <LoadingComponent/> }
 
     function seatAvailable(seatId) {
-        if (selectedSeats.includes(seatId)) {
-            return false; //assento já selecionado, precisa deselecionar
-        }
+        if (selectedSeats.includes(seatId)) return false; //assento já selecionado, precisa deselecionar
         return true; //assento não selecionado, selecionar
     }
 
@@ -47,7 +44,7 @@ export default function SeatsPage({setFinalInfos}) {
         inputValues[attribute] = event.target.value
         setInputValues({...inputValues});
     }
-    
+
     async function reserveSeats() {
         const seatsObj = {
             ids: selectedSeats,
@@ -71,6 +68,16 @@ export default function SeatsPage({setFinalInfos}) {
             console.log(err)
         }
     }
+
+    function isValid(str,type) {
+        if (type === "cpf") return regexCPF.test(str);
+        if (type === "name") return regexNOME.test(str);
+    }
+
+    function activateButton() {
+        return (selectedSeats.length && isValid(inputValues.name,"name") && isValid(inputValues.cpf,"cpf"))
+    }
+    
 
     return (
         <main className="main-content">
@@ -104,14 +111,17 @@ export default function SeatsPage({setFinalInfos}) {
                 {/* Exemplo de input controlado (pelo react) usando onChange e value
                     Desta forma caso o input seja renderizado ele não perderá o valor
                 */}
-                <input placeholder="Digite seu nome..." onChange={(e) => change(e,"name")} value={!inputValues.name ? "" : inputValues.name}/>
+                <Input isValid={isValid(inputValues.name,"name")} placeholder="Digite seu nome..." onChange={(e) => change(e,"name")} value={!inputValues.name ? "" : inputValues.name}/>
+                <p>{isValid(inputValues.name,"name") ? "" : "nome invalido"}</p>
             </section>
             <section  className="seatsPage-input">
                 <h3>CPF do comprador:</h3>
-                <input placeholder="Digite seu CPF..." onChange={(e) => change(e,"cpf")} value={!inputValues.cpf ? "" : inputValues.cpf}/>
+                <Input isValid={isValid(inputValues.cpf,"cpf")} placeholder="Digite seu CPF..." onChange={(e) => change(e,"cpf")} value={!inputValues.cpf ? "" : inputValues.cpf}/>
+                <p>{isValid(inputValues.cpf,"cpf") ? "" : "cpf invalido"}</p>
             </section>
 
-            <button className="seatsPage-button" onClick={reserveSeats}>Reservar assento(s)</button>
+            {/* styled component */}
+            <Button enabled={activateButton()} onClick={activateButton()  ? reserveSeats : ()=>{}}>Reservar assento(s)</Button>
 
             <Footer posterURL={seats.movie.posterURL} movieTitle={seats.movie.title} weekday={seats.day.weekday} time={seats.name}/>
         </main>
@@ -134,3 +144,32 @@ function Seat({id,isAvailable,name,toggleSeat}) {
         </div>
     )
 }
+
+const Button = styled.button`
+    background-color: #E8833A;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    border-radius: 3px;
+    cursor: pointer;
+    margin: 30px auto ;
+    font-size: 18px;
+    width: 225px;
+    height: 42px;
+    border: none;
+    opacity: ${({enabled}) => enabled ? 1 : 0.6};
+`
+
+const Input = styled.input`
+    width: 100%;
+    height: 51px;
+    border-radius: 3px;
+    border: 1px solid ${({isValid}) => isValid ? '#D5D5D5' : 'red'};
+    padding-left: 20px;
+    font-size: 18px;
+    
+    &:focus {
+        outline: none;
+    }
+`
